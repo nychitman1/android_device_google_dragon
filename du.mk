54f77dba46d859a654152d05f550d4a6dd73554e
Josh Chasky <jchasky@gmail.com>
2018-12-02 20:34:53 -0500

    Update build fingerprint to OPM8.181105.002
    Change-Id: If87d47b7596c64f467cb635c1c068581ee3ecfe7

--- du.mk
+++ du.mk
@@ -34,8 +34,8 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel C
 PRODUCT_MANUFACTURER := Google
 
-BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM8.181005.003/4984325:user/release-keys
+BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM8.181105.002/5038063:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=dragon \
-    PRIVATE_BUILD_DESC="ryu-user 8.1.0 OPM8.181005.003 4984325 release-keys"
+    PRIVATE_BUILD_DESC="ryu-user 8.1.0 OPM8.181105.002 5038063 release-keys"
